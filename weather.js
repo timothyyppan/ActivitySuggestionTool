c8333960feb5619700f969205ed8988f961aa525
timothyyppan <124206804+timothyyppan@users.noreply.github.com>
2023-02-01 17:35:18 -0500

    Got the variables to work

--- weather.js
+++ weather.js
@@ -3,14 +3,27 @@ const url = 'https://api.open-meteo.com/v1/forecast?latitude=44.23&longitude=-76
 Http.open("GET", url);
 Http.send();
 
-
 Http.onreadystatechange = function(){
     if(this.readyState == 4 && this.status == 200){
+
         var test = Http.responseText
         var betterTest = JSON.parse(test)
+        var latitude = betterTest.latitude
+        var longitude = betterTest.longitude
+        var currentTemperature = betterTest.current_weather.temperature
+        var currentTime = betterTest.current_weather.time
+        var currentWindDirection = betterTest.current_weather.winddirection
+        var currentWindSpeed = betterTest.current_weather.windspeed
+
+
+
         console.log(betterTest)
-        var latitude = betterTest.split(",");
-        console.log(latitude);
+        console.log(latitude)
+        console.log(longitude)
+        console.log(currentTemperature)
+        console.log(currentTime)
+        console.log(currentWindDirection)
+        console.log(currentWindSpeed)
     }
 }
 
