5df3c5219b9f6eb7304e53b82f4854f753d9c3f4
timothyyppan <124206804+timothyyppan@users.noreply.github.com>
2023-02-01 17:26:40 -0500

    Change

--- weather.js
+++ weather.js
@@ -3,8 +3,15 @@ const url = 'https://api.open-meteo.com/v1/forecast?latitude=44.23&longitude=-76
 Http.open("GET", url);
 Http.send();
 
+
 Http.onreadystatechange = function(){
     if(this.readyState == 4 && this.status == 200){
-        console.log(Http.responseText)
+        var test = Http.responseText
+        var betterTest = JSON.parse(test)
+        console.log(betterTest)
+        var latitude = betterTest.split(",");
+        console.log(latitude);
     }
 }
+
+
