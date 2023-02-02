bd5d59b11c8c5f658a83335c4ee5c98b7afd924a
timothyyppan <124206804+timothyyppan@users.noreply.github.com>
2023-02-01 21:32:32 -0500

    change

--- weather.js
+++ weather.js
@@ -84,47 +84,52 @@ Http.onreadystatechange = function(){
         console.log(currentWindDirection)
         console.log(currentWindSpeed)
 
-
-        for(let i = 0; i < fairActivities.length; i++){
-            console.log(fairActivities[i])
-        }
-
         if(currentTemperature > 30){
             console.log("Try to stay inside as it is too hot outside");
-            document.getElementById("ph").innerHTML = "<div class='hot'\> <span class='sun'></span\> <span class='sunx'></span\> </div>";
-            document.getElementById("ew").innerHTML = "<p>weather is too hot</p>";
+            document.getElementById("bubbleImage").innerHTML = "<div class='hot'\> <span class='sun'></span\> <span class='sunx'></span\> </div>";
+            document.getElementById("weatherStatement").innerHTML = "<p>weather is too hot</p>";
         }
         else if(currentTemperature >= 25 && currentTemperature <= 30){
+            /*
+            for(let i = 0; i < hotActivities.length(); i++){
+                document.getElementById("ew").innerHTML = "<p>weather is hot</p>";
+            }
+            */
             console.log(hotActivities);
-            document.getElementById("ph").innerHTML = "<div class='hot'\> <span class='sun'></span\> <span class='sunx'></span\> </div>";
-            document.getElementById("ew").innerHTML = "<p>weather is hot</p>";
+            document.getElementById("bubbleImage").innerHTML = "<div class='hot'\> <span class='sun'></span\> <span class='sunx'></span\> </div>";
+            document.getElementById("weatherStatement").innerHTML = "<p>weather is hot</p>";
         }
         else if(currentTemperature >= 15 && currentTemperature <= 25){
             console.log(warmActivities);
-            document.getElementById("ph").innerHTML = "<div class='hot'\> <span class='sun'></span\> <span class='sunx'></span\> </div>";
-            document.getElementById("ew").innerHTML = "<p>weather is warm</p>";
+            document.getElementById("bubbleImage").innerHTML = "<div class='hot'\> <span class='sun'></span\> <span class='sunx'></span\> </div>";
+            document.getElementById("weatherStatement").innerHTML = "<p>weather is warm</p>";
 
         }
         else if(currentTemperature >= 5 && currentTemperature <= 15){
             console.log(fairActivities);
-            document.getElementById("ph").innerHTML = " <div class='cloudy'\><span class='cloud'\></span\><span class='cloudx'\></span\></div\>";
-            document.getElementById("ew").innerHTML = "<p>weather is fair</p>";
+            document.getElementById("bubbleImage").innerHTML = " <div class='cloudy'\><span class='cloud'\></span\><span class='cloudx'\></span\></div\>";
+            document.getElementById("weatherStatement").innerHTML = "<p>weather is fair</p>";
         }
         else if(currentTemperature >= 0 && currentTemperature <= 5){
             console.log(chillyActivities);
-            document.getElementById("ph").innerHTML = " <div class='cloudy'\><span class='cloud'\></span\><span class='cloudx'\></span\></div\>";
-            document.getElementById("ew").innerHTML = "<p>weather is chilly</p>";
+            document.getElementById("bubbleImage").innerHTML = " <div class='cloudy'\><span class='cloud'\></span\><span class='cloudx'\></span\></div\>";
+            document.getElementById("weatherStatement").innerHTML = "<p>weather is chilly</p>";
         }
         else if(currentTemperature >= -15 && currentTemperature <= 0){
+            
+            for(let i = 0; i < coldActivities.length; i++){
+                document.getElementById("activities").innerHTML = "<p>coldActivities[i]</p>";
+            }
+            
             console.log(coldActivities);
-            document.getElementById("ph").innerHTML = "<div class='stormy'> <ul> <li></li> <li></li> <li></li> <li></li>  <li></li> <li></li> <li></li> </ul> <span class='snowe'></span> <span class='snowex'></span> <span class='stick'></span> <span class='stick2'></span> </div>";
-            document.getElementById("ew").innerHTML = "<p>weather is cold</p>";
+            document.getElementById("bubbleImage").innerHTML = "<div class='stormy'> <ul> <li></li> <li></li> <li></li> <li></li>  <li></li> <li></li> <li></li> </ul> <span class='snowe'></span> <span class='snowex'></span> <span class='stick'></span> <span class='stick2'></span> </div>";
+            document.getElementById("weatherStatement").innerHTML = "<p>weather is cold</p>";
         }
         
         if(currentTemperature < -15){
             console.log("Stay inside its too cold outside")
-            document.getElementById("ph").innerHTML = "<div class='stormy'> <ul> <li></li> <li></li> <li></li> <li></li> <li></li> <li></li> <li></li> </ul> <span class='snowe'></span> <span class='snowex'></span> <span class='stick'></span> <span class='stick2'></span> </div>";
-            document.getElementById("ew").innerHTML = "<p>Stay inside its too cold outside</p>";
+            document.getElementById("bubbleImage").innerHTML = "<div class='stormy'> <ul> <li></li> <li></li> <li></li> <li></li> <li></li> <li></li> <li></li> </ul> <span class='snowe'></span> <span class='snowex'></span> <span class='stick'></span> <span class='stick2'></span> </div>";
+            document.getElementById("weatherStatement").innerHTML = "<p>Stay inside its too cold outside</p>";
         
         }
         
