162d94253b540e8e3629928b44f83334de8d520a
timothyyppan <124206804+timothyyppan@users.noreply.github.com>
2023-02-01 23:54:43 -0500

    Changes

--- weather.js
+++ weather.js
@@ -1,8 +1,9 @@
-const Http = new XMLHttpRequest();
-const url = 'https://api.open-meteo.com/v1/forecast?latitude=44.23&longitude=-76.50&hourly=temperature_2m&current_weather=true';
-Http.open("GET", url);
-Http.send();
+const HttpWeather = new XMLHttpRequest();
+const urlWeather = 'https://api.open-meteo.com/v1/forecast?latitude=44.23&longitude=-76.50&hourly=temperature_2m&current_weather=true';
+HttpWeather.open("Get", urlWeather);
+HttpWeather.send();
 
+let str;
 const hotActivities = [
     "Kayaking",
     "Canoeing",
@@ -64,67 +65,71 @@ const chillyActivities = [
 
 ]
 
-Http.onreadystatechange = function(){
+HttpWeather.onreadystatechange = function(){
     if(this.readyState == 4 && this.status == 200){
 
-        var test = Http.responseText
-        var betterTest = JSON.parse(test)
-        var latitude = betterTest.latitude
-        var longitude = betterTest.longitude
-        var currentTemperature = betterTest.current_weather.temperature
-        var currentTime = betterTest.current_weather.time
-        var currentWindDirection = betterTest.current_weather.winddirection
-        var currentWindSpeed = betterTest.current_weather.windspeed
+        var weather = HttpWeather.responseText
+        var betterWeather = JSON.parse(weather)
+        var latitude = betterWeather.latitude
+        var longitude = betterWeather.longitude
+        var currentTemperature = betterWeather.current_weather.temperature
+        var currentTime = betterWeather.current_weather.time
+        var currentWindSpeed = betterWeather.current_weather.windspeed
 
-        console.log(betterTest)
-        console.log(latitude)
-        console.log(longitude)
-        console.log(currentTemperature)
-        console.log(currentTime)
-        console.log(currentWindDirection)
-        console.log(currentWindSpeed)
+        console.log(betterWeather);
+
+        document.getElementById("currentTemp").innerHTML = currentTemperature;
+        document.getElementById("windSpeed").innerHTML = currentWindSpeed;
 
         if(currentTemperature > 30){
-            console.log("Try to stay inside as it is too hot outside");
-            document.getElementById("bubbleImage").innerHTML = "<div class='hot'\> <span class='sun'></span\> <span class='sunx'></span\> </div>";
-            document.getElementById("weatherStatement").innerHTML = "<p>weather is too hot</p>";
+            //document.getElementById("bubbleImage").innerHTML = "<div class='hot'\> <span class='sun'></span\> <span class='sunx'></span\> </div>";
+            document.getElementById("weatherStatement").innerHTML = "<p>It is too hot outside</p>";
         }
         else if(currentTemperature >= 25 && currentTemperature <= 30){
-            console.log(hotActivities);
-            document.getElementById("bubbleImage").innerHTML = "<div class='hot'\> <span class='sun'></span\> <span class='sunx'></span\> </div>";
-            document.getElementById("weatherStatement").innerHTML = "<p>weather is hot</p>";
+            for(let i = 0; i < hotActivities.length; i++){
+                str = hotActivities[i] + " "
+                document.getElementById("activities").innerHTML += str
+            }
+            //document.getElementById("bubbleImage").innerHTML = "<div class='hot'\> <span class='sun'></span\> <span class='sunx'></span\> </div>";
+            document.getElementById("weatherStatement").innerHTML = "<p>It is hot outside</p>";
         }
         else if(currentTemperature >= 15 && currentTemperature <= 25){
-            console.log(warmActivities);
-            document.getElementById("bubbleImage").innerHTML = "<div class='hot'\> <span class='sun'></span\> <span class='sunx'></span\> </div>";
-            document.getElementById("weatherStatement").innerHTML = "<p>weather is warm</p>";
+            for(let i = 0; i < warmActivities.length; i++){
+                str = warmActivities[i] + " "
+                document.getElementById("activities").innerHTML += str
+            }
+            //document.getElementById("bubbleImage").innerHTML = "<div class='hot'\> <span class='sun'></span\> <span class='sunx'></span\> </div>";
+            document.getElementById("weatherStatement").innerHTML = "<p>It is warm outside</p>";
 
         }
         else if(currentTemperature >= 5 && currentTemperature <= 15){
-            console.log(fairActivities);
-            document.getElementById("bubbleImage").innerHTML = " <div class='cloudy'\><span class='cloud'\></span\><span class='cloudx'\></span\></div\>";
-            document.getElementById("weatherStatement").innerHTML = "<p>weather is fair</p>";
+            for(let i = 0; i < fairActivities.length; i++){
+                str = fairActivities[i] + " "
+                document.getElementById("activities").innerHTML += str
+            }
+            //document.getElementById("bubbleImage").innerHTML = " <div class='cloudy'\><span class='cloud'\></span\><span class='cloudx'\></span\></div\>";
+            document.getElementById("weatherStatement").innerHTML = "<p>It is fair outside</p>";
         }
         else if(currentTemperature >= 0 && currentTemperature <= 5){
-            console.log(chillyActivities);
-            document.getElementById("bubbleImage").innerHTML = " <div class='cloudy'\><span class='cloud'\></span\><span class='cloudx'\></span\></div\>";
-            document.getElementById("weatherStatement").innerHTML = "<p>weather is chilly</p>";
+            for(let i = 0; i < coldActivities.length; i++){
+                str = coldActivities[i] + " "
+                document.getElementById("activities").innerHTML += str
+            }
+            //document.getElementById("bubbleImage").innerHTML = " <div class='cloudy'\><span class='cloud'\></span\><span class='cloudx'\></span\></div\>";
+            document.getElementById("weatherStatement").innerHTML = "<p>It is cold outside</p>";
         }
         else if(currentTemperature >= -15 && currentTemperature <= 0){
-            
-            for(let i = 0; i < coldActivities.length; i++){
-                document.getElementById("activities").innerHTML = "<p>coldActivities[i]</p>";
+            for(let i = 0; i < chillyActivities.length; i++){
+                str = chillyActivities[i] + " "
+                document.getElementById("activities").innerHTML += str
             }
-            
-            console.log(coldActivities);
-            document.getElementById("bubbleImage").innerHTML = "<div class='stormy'> <ul> <li></li> <li></li> <li></li> <li></li>  <li></li> <li></li> <li></li> </ul> <span class='snowe'></span> <span class='snowex'></span> <span class='stick'></span> <span class='stick2'></span> </div>";
-            document.getElementById("weatherStatement").innerHTML = "<p>weather is cold</p>";
+            //document.getElementById("bubbleImage").innerHTML = "<div class='stormy'> <ul> <li></li> <li></li> <li></li> <li></li>  <li></li> <li></li> <li></li> </ul> <span class='snowe'></span> <span class='snowex'></span> <span class='stick'></span> <span class='stick2'></span> </div>";
+            document.getElementById("weatherStatement").innerHTML = "<p>It is chilly outside</p>";
         }
         
         if(currentTemperature < -15){
-            console.log("Stay inside its too cold outside")
-            document.getElementById("bubbleImage").innerHTML = "<div class='stormy'> <ul> <li></li> <li></li> <li></li> <li></li> <li></li> <li></li> <li></li> </ul> <span class='snowe'></span> <span class='snowex'></span> <span class='stick'></span> <span class='stick2'></span> </div>";
-            document.getElementById("weatherStatement").innerHTML = "<p>Stay inside its too cold outside</p>";
+            //document.getElementById("bubbleImage").innerHTML = "<div class='stormy'> <ul> <li></li> <li></li> <li></li> <li></li> <li></li> <li></li> <li></li> </ul> <span class='snowe'></span> <span class='snowex'></span> <span class='stick'></span> <span class='stick2'></span> </div>";
+            document.getElementById("weatherStatement").innerHTML = "<p>It is too cold outside</p>";
         
         }
         
